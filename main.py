ad17c915e9a8046ec17fb2757709b7da0d7bd717
hanss314 <24215006+hanss314@users.noreply.github.com>
2017-11-06 21:54:24 -0800

    Got oauth2 working BTW, on the applications/me page, add "http://localhost:8080/queue/oauth2" to the redirect uris for the application

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
+import os
 import json
 
+from ruamel import yaml
+
 from bottle import Bottle, run, template, static_file, request, redirect
+from requests_oauthlib import OAuth2Session
+
+with open('./config/config.yml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+OAUTH2_CLIENT_ID = config['id']
+OAUTH2_CLIENT_SECRET = config['secret']
 
 
-REDIRECT = 'http%3A%2F%2F127.0.0.1%3A8080%2Fqueue%2Foauth2'
+REDIRECT = 'http://localhost:8080/queue/oauth2'
 OAUTH2 = 'https://discordapp.com/api/oauth2/authorize?response_type=code&client_id=377169849744359424&scope=identify&state={state}&redirect_uri=' + REDIRECT
+
+if 'http://' in REDIRECT:
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
 
 keys = {
 
 }
 
+# I'm using this for now, does bottle have sessions? I couldn't find it in the docs
+sessions = {
+    
+}
+
 
 app = Bottle()
+
+
+def make_session(token=None, state=None, scope=None):
+     return OAuth2Session(
+        client_id=OAUTH2_CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=REDIRECT,
+        auto_refresh_kwargs={
+            'client_id': OAUTH2_CLIENT_ID,
+            'client_secret': OAUTH2_CLIENT_SECRET,
+        },
+        auto_refresh_url='https://discordapp.com/api/oauth2/token'
+        )
 
 queue = [
     ('Song name'*10, 'jim'),
@@ -23,6 +56,14 @@ queue = [
 
 @app.get('/queue/oauth2')
 def oauth2_complete():
+    discord = make_session(state=sessions[request.remote_addr].get('oauth2_state'))
+    token = discord.fetch_token(
+        'https://discordapp.com/api/oauth2/token',
+        client_secret=OAUTH2_CLIENT_SECRET,
+        authorization_response=request.url
+    )
+    sessions[request.remote_addr]['oauth2_token'] = token
+
     key = request.query['code']
 
     keys[request.remote_addr] = key
@@ -34,8 +75,19 @@ def oauth2_complete():
 @app.get('/queue')
 def queue_requested():
     if request.remote_addr not in keys:
-        redirect(OAUTH2.format(state='15773059ghq9183habn'))
+        discord = make_session(scope='identify guilds')
+        authorization_url, state = discord.authorization_url(
+            'https://discordapp.com/api/oauth2/authorize')
+        sessions[request.remote_addr] = {}
+        sessions[request.remote_addr]['oauth2_state'] = state
+        redirect(authorization_url)
         return
+
+    discord = make_session(token=sessions[request.remote_addr].get('oauth2_token'))
+    user = discord.get('https://discordapp.com/api/users/@me').json()
+    guilds = discord.get('https://discordapp.com/api/users/@me/guilds').json()
+    print(user)
+    print(guilds)
 
     key = keys[request.remote_addr]
     return template('queue', {'key': key})
@@ -72,5 +124,6 @@ def api_request():
 def stylesheets(filename):
     return static_file(filename, root='static/')
 
+
 if __name__ == '__main__':
-    run(app, host='0.0.0.0', port=8080, debug=True, reloader=True)
+    run(app, host='127.0.0.1', port=8080, debug=True, reloader=True)
